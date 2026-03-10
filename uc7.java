b3a3d630ca8ee9d5180709fcbe34daaac689bc65
sa8959-cloud <sa8959@srmist.edu.in>
2026-03-10 08:21:45 +0530

    Add files via upload

---
+++ uc7.java
@@ -0,0 +1,60 @@
+public class uc7 {
+
+    static class CharacterPattern {
+        char character;
+        String[] pattern;
+
+        CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        CharacterPattern O = new CharacterPattern('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        CharacterPattern P = new CharacterPattern('P', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        CharacterPattern S = new CharacterPattern('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        });
+
+        String[] word = {"O", "O", "P", "S"};
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(
+                    O.getPattern()[i] + " " +
+                    O.getPattern()[i] + " " +
+                    P.getPattern()[i] + " " +
+                    S.getPattern()[i]
+            );
+        }
+    }
+}
